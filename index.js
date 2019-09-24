beb678c127035b8084029bfb16c423d130a5e75d
fistonhn <fistonhn@gmail.com>
2019-09-24 23:26:51 +0200

    add user

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', (req, res) => {
     res.send('it is working');
 });
 
+
+app.get('/user', (req, res) => {
+    res.send('fiston is coming');
+});
+
 app.listen(3000, () => { console.log('listening on port 30000')});
